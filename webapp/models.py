3cd1160b11fe07f4202ff0ee4c76ee231dc5e9c4
annawu030 <yw6fy@virginia.edu>
2019-09-23 07:52:03 -0400

    update model for reset password

--- webapp/models.py
+++ webapp/models.py
@@ -11,7 +11,7 @@ class User_account(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    manager_status = db.Column(db.Integer, default=0)
+    manager_status = db.Column(db.Boolean, default=False)
     assessments = db.relationship('Assessment', backref='user_account', lazy='dynamic')
     assessment_templates = db.relationship('AssessmentTemplate', backref='user_account', lazy='dynamic')
 
