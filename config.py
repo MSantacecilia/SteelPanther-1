02529aa6b1d378d1752b8ac211171a6e442e84b7
Ford Hendrix (Strategic Engagements) <FO20107235@wipro.com>
2019-09-25 16:40:32 -0400

    new index file

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL') or 'postgres+psycopg2://mukulbatra:Cleaver@54321@localhost:5432/mydb'
+        'DATABASE_URL') or 'postgres+psycopg2://postgres:C0wboybebop!@localhost:2345/mydb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
