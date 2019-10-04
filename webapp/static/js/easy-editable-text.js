7040a5d762b324a85a64eabcae6ce4e9a09debc7
Ford Hendrix (Strategic Engagements) <FO20107235@wipro.com>
2019-10-04 11:46:09 -0400

    default value bug fix

--- webapp/static/js/easy-editable-text.js
+++ webapp/static/js/easy-editable-text.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	
+
 	$('.edit').click(function(){
 		$(this).hide();
 		$(this).parent().prev().children().first().hide();
@@ -7,39 +7,41 @@ $(document).ready(function(){
 		$(this).parent().prev().children().first().next().show();
 		$(this).parent().prev().children().first().next().select();
 	});
-	
-	
+
+
 	$('input[type="text"]').blur(function() {
          if ($.trim(this.value) == ''){
-			 this.value = (this.defaultValue ? this.defaultValue : '');  
+			 this.value = (this.defaultValue ? this.defaultValue : '');
 		 }
 		 else{
 			 $(this).prev().html(this.value);
 			 // TODO send this.value to database
+			 this.defaultValue=this.value;
 		 }
-		 
+
 		 $(this).hide();
 		 $(this).next().hide();
 		 $(this).prev().show();
 		 $(this).parent().next().children().first().next().show();
      });
-	  
+
 	  $('input[type="text"]').keypress(function(event) {
 		  if (event.keyCode == '13') {
-			  if ($.trim(this.value) == ''){  
-				 this.value = (this.defaultValue ? this.defaultValue : '');  
+			  if ($.trim(this.value) == ''){
+				 this.value = (this.defaultValue ? this.defaultValue : '');
 			 }
 			 else
 			 {
 				 $(this).prev().html(this.value);
 				 // TODO send this.value to database
+				 this.defaultValue=this.value;
 			 }
-			 
+
 			 $(this).hide();
 			 $(this).next().hide();
 			 $(this).prev().show();
 			 $(this).parent().next().children().first().next().show();
 		  }
 	  });
-		  
+
 });
